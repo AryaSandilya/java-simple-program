b51c7dc8ff272c6c13c56e6b0fd84a1064ac474e
Sojourner <aryasandilya41@gmail.com>
2026-02-26 19:50:33 +0530

    Created an object

--- javaprogram.java
+++ javaprogram.java
@@ -2,5 +2,20 @@ class javaprogram {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Hello, World again!");
+        MyObject obj = new MyObject("This is an object message");
+        System.out.println(obj);
+    }
+}
+
+class MyObject {
+    private String message;
+
+    public MyObject(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "MyObject: " + message;
     }
 }
